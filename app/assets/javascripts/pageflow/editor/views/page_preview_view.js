1b254c9f5a7dcf3155bdb5fce208b4d6b872403c
Tim Fischbach <tfischbach@codevise.de>
2015-01-10 12:21:22 +0100

    Improve Page Type Editor Api
    Extract editor specific methods from page type and allow registering
    via editor api instead.

--- app/assets/javascripts/pageflow/editor/views/page_preview_view.js
+++ app/assets/javascripts/pageflow/editor/views/page_preview_view.js
@@ -59,7 +59,7 @@ pageflow.PagePreviewView = Backbone.Marionette.View.extend({
   update: function() {
     this.$el.removeClass(pageflow.Page.transitions.join(' ')).addClass(this.model.configuration.get('transition'));
 
-    this.pageType().update(this.$el, this.model.configuration);
+    this.pageTypeHooks().update(this.$el, this.model.configuration);
     _.extend(this.$el.data('configuration'), this.model.configuration.attributes);
 
     this.refreshScroller();
@@ -71,7 +71,7 @@ pageflow.PagePreviewView = Backbone.Marionette.View.extend({
     this.$el.toggleClass('chapter_beginning', chapterBeginning);
   },
 
-  pageType: function() {
+  pageTypeHooks: function() {
     return this.$el.data('pageType');
   },
 
@@ -96,7 +96,7 @@ pageflow.PagePreviewView = Backbone.Marionette.View.extend({
 
     view.embeddedViews = new Backbone.ChildViewContainer();
 
-    _.each(view.pageType().embeddedEditorViews(), function(item, selector) {
+    _.each(view.embeddedViewDefinitions(), function(item, selector) {
       view.$(selector).each(function() {
         view.embeddedViews.add(new item.view(_.extend(item.options || {}, {
           el: this,
@@ -105,6 +105,14 @@ pageflow.PagePreviewView = Backbone.Marionette.View.extend({
         })).render());
       });
     });
+  },
+
+  embeddedViewDefinitions: function() {
+    return _.extend(
+      {},
+      this.pageTypeHooks().embeddedEditorViews() || {},
+      this.model.pageType().embeddedViews()
+    );
   },
 
   _unescape: function(text) {

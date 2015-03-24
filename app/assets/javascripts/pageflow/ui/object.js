ac97670de4a1407b7f4af0d05df2c4ab3b982c00
Tim Fischbach <tfischbach@codevise.de>
2015-03-24 17:54:55 +0100

    Require pageflow/object from pageflow/ui/object
    For backward compatibility.

--- app/assets/javascripts/pageflow/ui/object.js
+++ app/assets/javascripts/pageflow/ui/object.js
@@ -1,3 +1,5 @@
 // pageflow.Object is now part of pageflow/base.js and no longer needs
 // to be required by Pageflow extensions. Keep this file for backwards
 // compatibility.
+
+//= require pageflow/object

47ecbe7cfdf2ec50512068241b2d25ce8e13bb60
Tim Fischbach <mail@timfischbach.de>
2015-08-30 14:29:09 +0200

    Storyline refactoring
    Introduce `Storyline` model as new container for chapters inside
    revisions. Create a main storyline by default and provide JSON Api
    endpoints to manage storylines.

---
+++ app/models/pageflow/storyline.rb
@@ -0,0 +1,21 @@
+module Pageflow
+  class Storyline < ActiveRecord::Base
+    include RevisionComponent
+
+    has_many :chapters
+    has_many :pages, through: :chapters
+
+    delegate :entry, to: :revision
+
+    serialize :configuration, JSON
+
+    def copy_to(revision)
+      storyline = dup
+      revision.storylines << storyline
+
+      chapters.each do |chapter|
+        chapter.copy_to(storyline)
+      end
+    end
+  end
+end
